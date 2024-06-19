7bbde15b9bff50fc6cd3f1ca072a559d2b383fd4
Headsh0t <88953209+Ziedny28@users.noreply.github.com>
2024-06-19 11:42:48 +0700

    added timer manager and endgame

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI _timerTMPro;
+    [SerializeField] private float _time = 60;
+    [SerializeField] private Animator _camAnimator;
+    [SerializeField] private GameObject _boy;
+    [SerializeField] private EnemySpawnerManager _enemySpawnerManager;
+    [SerializeField] private string _boyGameOverAnimTriggerName = "EndGame";
+    [SerializeField] private string _camGameOverAnimTriggerName = "EndGame";
+    private Animator _boyAnimator;
+    private bool isGameOver = false;
+
+    private void Awake()
     {
-        
+        UpdateTimerText();
+        InvokeRepeating(nameof(DecreaseTime), 1, 1);
+
+    }
+    private void DecreaseTime()
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+        _time -= 1;
+        UpdateTimerText();
+        if (_time == 0)
+        {
+            EndGame();
+            isGameOver = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTimerText()
     {
-        
+        _timerTMPro.text = $"Timer:{_time}";
+    }
+
+
+    private void EndGame()
+    {
+        Debug.Log("EndGame");
+        SetupBoyEndGame();
+        _camAnimator.SetTrigger(_camGameOverAnimTriggerName);
+        _enemySpawnerManager.enabled = false;
+    }
+
+    private void SetupBoyEndGame()
+    {
+        PlayerController boyController = _boy.GetComponent<PlayerController>();
+        boyController.enabled = false;
+        PlayerShooter boyShooter = _boy.GetComponent<PlayerShooter>();
+        boyShooter.enabled = false;
+        Animator boyAnimator = _boy.GetComponent<Animator>();
+        boyAnimator.SetTrigger(_boyGameOverAnimTriggerName);
     }
 }
